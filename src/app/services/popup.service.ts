3c017485936f9966c09cc95ceb189eb489afdd11
yochairo <joromabe@gmail.com>
2021-04-18 16:48:47 -0400

    news and covid-19 data added

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -17,7 +17,6 @@ export class PopupService {
       `<div>Population: ${ data.population }</div>
        <a mat-raised-button href="http://localhost:4200/main/graphics" target="_blank">Mas info</a>`;
        }
-  // tslint:disable-next-line:typedef
   listdatos(iddepa: number): Dato[] {
 this.datosService.getgenneralsumdep(iddepa).subscribe((data) => {
   this.datosum = data;
