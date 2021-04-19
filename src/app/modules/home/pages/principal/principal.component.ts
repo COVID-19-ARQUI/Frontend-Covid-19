77925d775bedc07d90c2f544cb8425187054a328
yochairo <joromabe@gmail.com>
2021-04-19 16:01:28 -0400

    depar vac corected added

--- src/app/modules/home/pages/principal/principal.component.ts
+++ src/app/modules/home/pages/principal/principal.component.ts
@@ -28,6 +28,8 @@ L.Marker.prototype.options.icon = iconDefault;
 export class PrincipalComponent implements OnInit {
   databolivia: Dato[] = [];
   lista: any;
+  listav1: any;
+  listav2: any;
   private map;
   ndata: number[] = [];
   nddata: number[] = [];
@@ -53,6 +55,8 @@ export class PrincipalComponent implements OnInit {
      this.initMap();
     // this.markerService.makeCapitalMarkers(this.map);
      this.listdatos();
+     this.listdatosv1();
+    this.listdatosv2();
      for (let _i = 0; _i < 9; _i++){
        this.listdatodepa(_i);
      }
@@ -61,8 +65,20 @@ export class PrincipalComponent implements OnInit {
      // console.log(this.lista);
   }
   listdatos(): any{
-    this.datosService.getgenneralsum().subscribe(value => {
+    this.datosService.getgenneralvaccine().subscribe(value => {
       this.lista = value;
+    });
+    return this.lista;
+  }
+  listdatosv1(): any{
+    this.datosService.getgenneralvaccine2().subscribe(value => {
+      this.listav1 = value;
+    });
+    return this.lista;
+  }
+  listdatosv2(): any{
+    this.datosService.getgenneralsum().subscribe(value => {
+      this.listav2 = value;
     });
     return this.lista;
   }
