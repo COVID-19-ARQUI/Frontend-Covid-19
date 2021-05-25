82261651b3f218be773e01a66324521c3ad8d044
yochairo <joromabe@gmail.com>
2021-05-25 12:30:50 -0400

    edit finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
@@ -18,6 +18,7 @@ import {AuthModule} from '@auth0/auth0-angular';
     BrowserModule,
     AppRoutingModule,
     HomeModule,
+    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AuthModule.forRoot({
