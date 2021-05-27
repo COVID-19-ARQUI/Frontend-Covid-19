25d46b8cf00df0281691489ad6ca50d6d23141cb
Savitar465 <55471147+Savitar465@users.noreply.github.com>
2021-05-26 23:43:57 -0400

    Log-in some fixes added

--- src/app/modules/home/pages/profile/profile.component.ts
+++ src/app/modules/home/pages/profile/profile.component.ts
@@ -8,13 +8,12 @@ import {AuthService} from '@auth0/auth0-angular';
 })
 export class ProfileComponent implements OnInit {
   profileJson: string=null;
-  constructor(public auth:AuthService) { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
     this.auth.user$.subscribe((profile)=>(
       this.profileJson=JSON.stringify(profile,null,2)
     ))
-    console.log(this.auth.user$)
   }
 
 }
